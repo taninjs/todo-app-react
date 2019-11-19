9c809f8dfba26d8076e66bbf57a38d9c1ff0fd80
Tanin Thanasopon <>
2019-11-19 15:16:16 +0700

    add todo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <li><input type="checkbox"/>
-          Todo 1
-          <button>x</button>
-        </li>
-        <li><input type="checkbox"/> Todo 2<button>x</button></li>
-        <li><input type="checkbox"/> Todo 3<button>x</button></li>
-        <li><input type="checkbox"/> Todo 4<button>x</button></li>
-        <li><input type="checkbox"/> Todo 5<button>x</button></li>
-        <li><input type="checkbox"/> Todo 6<button>x</button></li>
-      </ul>
+class App extends React.Component {
 
-      <form>
-        <input type="text"/>
-        <input type="submit" value="ADD"/>
-      </form>
-    </div>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        { id: 1, text: 'Todo1', isCompleted: true },
+        { id: 2, text: 'Todo2', isCompleted: false },
+        { id: 3, text: 'Todo3', isCompleted: true },
+        { id: 4, text: 'Todo4', isCompleted: false },
+        { id: 5, text: 'Todo5', isCompleted: false },
+        { id: 6, text: 'Todo6', isCompleted: false },
+      ],
+      newTodoText: '',
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+    let newTodos = [
+      ...this.state.todos, 
+      { id: 10, text: this.state.newTodoText, isCompleted: false}
+    ]
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map(todo => (
+            <li key={todo.id}>
+              <input type="checkbox" checked={todo.isCompleted}/>
+              {todo.text}
+              <button>x</button>
+            </li>
+          ))}
+        </ul>
+  
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            value={this.state.newTodoText}
+            onChange={
+              (e) => this.setState({newTodoText: e.target.value})
+            }
+          />
+          <input type="submit" value="ADD"/>
+        </form>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
